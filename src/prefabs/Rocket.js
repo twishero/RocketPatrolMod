3c5f39ac7b2b8db17fe3c1faef16f1120038c120
Thane Wisherop <twishero@ucsc.edu>
2020-04-13 18:17:55 -0700

    Finish

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -2,9 +2,9 @@
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
-
         scene.add.existing(this); // add object to existing scene
-        this.isFiring - false; //track rockets firing status
+        this.isFiring = false; //track rockets firing status
+        this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
     }
 
     update() {
@@ -19,15 +19,19 @@ class Rocket extends Phaser.GameObjects.Sprite {
         // fire button
         if(Phaser.Input.Keyboard.JustDown(keyF)){
             this.isFiring = true;
+            this.sfxRocket.play(); // play sfx
         }
         // if fired, move up
-        if(this.isFirind && this.y >= 108) {
+        if(this.isFiring && this.y >= 108) {
             this.y -=2;
         }
         // reset on miss
         if(this.y <= 108) {
-            this.isfiring = false;
-            this.y = 431;
+            this.reset();
         }
+    }
+    reset(){
+        this.isFiring = false;
+        this.y = 431;
     }
 }
